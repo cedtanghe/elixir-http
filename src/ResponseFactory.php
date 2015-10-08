4627906f85b2f388e3369ff7067fc6e9fa2c5d1d
cedtanghe <ced.tanghe@gmail.com>
2015-10-08 18:38:50 +0200

    work in progress

--- src/ResponseFactory.php
+++ src/ResponseFactory.php
@@ -3,6 +3,10 @@
 namespace Elixir\HTTP;
 
 use Elixir\HTTP\Response;
+use Elixir\HTTP\Stream;
+use Psr\Http\Message\ResponseInterface as PSRResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\UriInterface;
 
 /**
  * @author Cédric Tanghe <ced.tanghe@gmail.com>
@@ -10,6 +14,64 @@ use Elixir\HTTP\Response;
 
 class ResponseFactory
 {
+    /**
+     * @param ServerRequestInterface $request
+     * @return boolean
+     */
+    public static function isNotModified(ServerRequestInterface $request)
+    {
+        $lastModified = $this->getHeaderLine('Last-Modified');
+        $ifModifiedSince = $request->getHeaderLine('If-Modified-Since');
+        
+        if(null !== $lastModified && null !== $ifModifiedSince)
+        {
+            if($lastModified === strtotime($ifModifiedSince))
+            {
+                return true;
+            }
+        }
+        
+        $etag = $this->getHeaderLine('Etag');
+        $ifNoneMatch = $request->getHeaderLine('If-None-Match');
+        
+        if(null !== $etag && null !== $ifNoneMatch)
+        {
+            if($etag === $ifNoneMatch)
+            {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    /**
+     * @param PSRResponseInterface|null $response
+     * @return PSRResponseInterface
+     */
+    public static function setNotModified(PSRResponseInterface $response = null)
+    {
+        if (null === $response)
+        {
+            $response = static::create([
+                'status_code' => 304,
+                'body_content' => null
+            ]);
+        }
+        else
+        {
+            $response = $response->withStatus(304);
+            $response = $response->withBody(new Stream('php://temp', 'r'));
+
+            foreach (array('Allow', 'Content-Encoding', 'Content-Language', 'Content-Length', 'Content-MD5', 'Content-Type', 'Last-Modified') as $header)
+            {
+                $response = $response->withoutHeader($header);
+            }
+        }
+        
+        return $response;
+    }
+    
     /**
      * @param string $string
      * @return Response
@@ -58,5 +120,72 @@ class ResponseFactory
             'protocol' => $protocol,
             'headers' => $headers
         ]);
+    }
+    
+    /**
+     * @param array $config
+     * @return Response
+     */
+    public static function create(array $config = [])
+    {
+        return new Response($config);
+    }
+    
+    /**
+     * @param string $HTML
+     * @param int $status
+     * @param array $config
+     * @return Response
+     */
+    public static function createHTML($HTML, $status = 200, array $config = [])
+    {
+        $config['body_content'] = $HTML;
+        $config['status_code'] = $status;
+        $config['headers']['content-type'] = ['text/html; charset=UTF-8'];
+        
+        return static::create($config);
+    }
+    
+    /**
+     * @param mixed $data
+     * @param int $status
+     * @param int $encodingOptions
+     * @param array $config
+     * @return Response
+     */
+    public static function createJSON($data, $status = 200, $encodingOptions = 0, array $config = [])
+    {
+        json_encode(null);
+        $JSON = json_encode($data, $encodingOptions);
+        
+        $config['body_content'] = $JSON;
+        $config['status_code'] = $status;
+        $config['headers']['content-type'] = ['application/json'];
+        
+        return static::create($config);
+    }
+    
+    /**
+     * @param string|UriInterface $URI
+     * @param int $status
+     * @param array $config
+     * @param boolean $send
+     * @return Response
+     */
+    public static function createRedirect($URI, $status = 302, array $config = [], $send = true)
+    {
+        $config['body_content'] = null;
+        $config['status_code'] = $status;
+        $config['headers']['location'] = [(string)$URI];
+        
+        $response = new Response($config);
+        
+        if ($send)
+        {
+            $response->send();
+            exit();
+        }
+        
+        return $response;
     }
 }
